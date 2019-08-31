6adf5643216b55a8d295843c2ad720ea4ec28655
JohnKostanov <kostanovd@icloud.com>
2019-09-01 00:30:16 +0200

    Add model MenuItem and MenuItems

---
+++ Restaurant/Models/MenuItem.swift
@@ -0,0 +1,27 @@
+//
+//  MenuItem.swift
+//  Restaurant
+//
+//  Created by  Джон Костанов on 01/09/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import Foundation
+
+struct MenuItem: Codable {
+    var id: Int
+    var name: String
+    var detailText: String
+    var price: Double
+    var category: String
+    var imageURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case detailText = "description"
+        case price
+        case category
+        case imageURL = "image_url"
+    }
+}
---
+++ Restaurant/Models/MenuItems.swift
@@ -0,0 +1,13 @@
+//
+//  MenuItems.swift
+//  Restaurant
+//
+//  Created by  Джон Костанов on 01/09/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import Foundation
+
+struct MenuItems: Codable {
+    let items: [MenuItem]
+}
