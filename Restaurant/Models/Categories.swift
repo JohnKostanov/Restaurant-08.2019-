35e0b24b4244892ae66e22ff8572d5152b958017
JohnKostanov <kostanovd@icloud.com>
2019-08-31 21:59:51 +0200

    Add Categories

---
+++ Restaurant/Models/Categories.swift
@@ -0,0 +1,13 @@
+//
+//  Categories.swift
+//  Restaurant
+//
+//  Created by  Джон Костанов on 31/08/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import Foundation
+
+struct Categories: Codable {
+    let categories: [String]
+}
