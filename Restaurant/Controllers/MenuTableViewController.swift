741761389bec92ec52d779b8ff9c52997f81c74d
JohnKostanov <kostanovd@icloud.com>
2019-08-31 22:33:07 +0200

    Add cell

--- Restaurant/Controllers/MenuTableViewController.swift
+++ Restaurant/Controllers/MenuTableViewController.swift
@@ -9,20 +9,38 @@
 import UIKit
 
 class MenuTableViewController: UITableViewController {
-    let networkManager = NetworkManager()
     
+    //MARK: - Properties
+    let networkManager = NetworkManager()
+    var categories = [String]()
+    
+    //MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         networkManager.getCategories { (categories, error) in
-            if let categories = categories {
-                print(#line, #function, categories)
-            } else {
+            guard let categories = categories else {
                 if let error = error {
                     print(#line, #function, "ERROR:", error.localizedDescription)
                 } else {
                     print(#line, #function, "ERROR: Can't load categories")
                 }
+                return
+            }
+            
+            self.categories = categories
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
             }
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row]
+        return cell
     }
 }
---
+++ Restaurant/Manager/CellManager.swift
@@ -0,0 +1,9 @@
+//
+//  CellManager.swift
+//  Restaurant
+//
+//  Created by  Джон Костанов on 31/08/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import Foundation
