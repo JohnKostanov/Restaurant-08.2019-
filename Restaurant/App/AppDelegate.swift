0cb520e16db41dee772f57a70bb556bf18ea5c6e
JohnKostanov <kostanovd@icloud.com>
2019-09-04 00:16:54 +0200

    Add orderTabBarItem.badgeValue

--- Restaurant/App/AppDelegate.swift
+++ Restaurant/App/AppDelegate.swift
@@ -12,5 +12,27 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var orderTabBarItem: UITabBarItem!
+    
+    @objc func updateOrderBadge() {
+        let count = OrderManager.shared.order.menuItems.count
+        orderTabBarItem.badgeValue = 0 < count ? "\(count)" : nil
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        let tabBarController = window?.rootViewController as! UITabBarController
+        orderTabBarItem = tabBarController.viewControllers![1].tabBarItem
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(updateOrderBadge),
+            name: OrderManager.orderUpdatedNotifacation,
+            object: nil
+        )
+        
+        return true
+    }
 }
 
