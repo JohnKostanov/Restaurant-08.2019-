1d081105b00546d92056ce31d33c0cb7976d5871
JohnKostanov <kostanovd@icloud.com>
2019-09-04 13:57:19 +0200

    Add fix PreparationTime

--- Restaurant/Models/PreperationTime.swift
+++ Restaurant/Models/PreperationTime.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct PreparationTime: Decodable {
+struct PreparationTime: Codable {
     let prepTime: Int
     enum CodingKeys: String, CodingKey {
         case prepTime = "preparation_time"
